1f58a900ce776bfae8c0fb062457bb9d5948065b
Mark <marklajom.work@gmail.com>
2024-03-26 10:01:44 +0100

    add citylist and cityitem

--- world-wise/src/components/CityList.jsx
+++ world-wise/src/components/CityList.jsx
@@ -1,6 +1,20 @@
+/* eslint-disable react/prop-types */
+import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
-function CityList() {
-  return <ul className={styles.cityList}>LIST</ul>;
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length) return <Message message="Add a city" />;
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default CityList;
